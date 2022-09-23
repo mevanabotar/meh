9dfdda7627d353c639bd042d704166c19e8a4aa3
mevana botar <mevanabotar@gmail.com>
2022-09-23 10:31:40 -0500

    deleted: todoes, dead code. added tests for ansi compilation

--- iris/iris.py
+++ iris/iris.py
@@ -83,94 +83,9 @@ def wash_brains(brain):
 def is_styled_nest(member):
  return member[0] == '[' and member[-1] == ']'
 
-def inherit(lsta, lstb):
- #lstb inherits from lsta. values in lstb overrite values in lsta.
- #[1,0,0,1,2,1] [None,None,3,None,4] -> [1,0,3,1,4,1]
- retn = []
- for i in range(len(lsta)):
-  try:
-   if lstb[i] is None:
-    retn.append(lsta[i])
-   else:
-    retn.append(lstb[i])
-  except IndexError:
-   return retn + lsta[i:]
- return retn
-
 """
 [(parent,style) [(child,style) chile] atom style]
-
-TODO:
-
-get the first style, the root style, from the style string. the remaining style string should still be valid. like
- [(ffffff,000000) white over black [(,310000) white over dark red]] ->
- (ffffff,000000) [() white over black [(,310000) white over dark red]] or
- (ffffff,000000) [(ffffff,000000) white over black [(,310000) white over dark red]]
-the point is this: I don't need a special case for when the stack is empty, since the stack never starts empty.
-
-create the next styledish by inheriting from the previous styledish
- stack: (ffffff,000000,1,0) (,,10,1) &
- stack: (ffffff,000000,1,0) (ffffff,ffffff,10,1)
-
-render the stylestring into ansi escape codes
- [(ff0000,222222,1) bold red over dark gray] ->
- \033[38;2;255;0;0m\033[48;2;22;22;22mbold red over dark gray
-
-render current style on entering nesting
- (ffffff,000000,1) [(,,0,1) non bold italicized]
- (ffffff,000000,1) (ffffff,000000,0,1)
-                   non bold italicized
-
-restore previous style on exit nesting
- (ffffff,000000,1) [(,,0,1) non bold italicized] white bold
- (ffffff,000000,1) (ffffff,000000,0,1)
-                   non bold italicized
- white bold
-white                 black           white                 black           bold   reset  white                 black           italic                    reset  white                 black           bold
-\033[38;2;255;255;255m\033[48;2;0;0;0m\033[38;2;255;255;255m\033[48;2;0;0;0m\033[1m\033[0m\033[38;2;255;255;255m\033[48;2;0;0;0m\033[3mnon bold italicized\033[0m\033[38;2;255;255;255m\033[48;2;0;0;0m\033[1m
-
-dog, these format strings are effing long! make an optimizing renderer...
- selective reset of characteristics: instead of blasting all style with \033[0m, undo specific styles as required
-  unbolden, unitalic, ununderlined. instead of uneverything and redo
-  (white,black,bold) (,,0,italic) ->
-  white black bold unbold italic
- smush inherited styles
-  (white,black,bold) (,,,underlined) ->
-  white black bold underlined
-
-  white black bold white black bold underlined
-
-
-
-Glossary:
- styledish: each level in the style stack
- stylestring: the nested representation of a markup string, [(val,val) string string [(val) string] [(,val) string]]
- basal: closer to the root
- apical: closer to the leafs
-
 """
-
-"""
-if current_stylenest is empty:
- clean your stack changes
- 
-
-if  the current element  is  a stylenest:
- save its styling info into the stylestacks
- save the rest of the current stylenest to the styletree stack
- current_stylenest = dismember(current element)
-else:
- render it with the current style
-"""
-
-tstsave = """[(ffffff,000000,1) this is no orthodox [(,555555) beheading] [(,,0) I'm cuting you [(ff0000) up] cutting you up] cutting you up will be so refreshing to me]"""
-
-tstcompilation = [
-        '[(ff0000,440000,1) bold red on dark red]',
-        '[(00ff00,440000,1) Im green over dark red and bold! [(0000ff) but I feel all blue]]',
-        '[(ff00ff,000000) [(,bbbb00) magenta over yellow] [(,00bbbb) magenta over cyan] [(,0000bb) magenta over blue] just magenta]',
-        '[(ffffff,007700,0,0) white over green darkish [(,,1) Im just bold [(,,,1) Im bold and italic!] just bold again] Ive been restored to my previous glory]'
-        ]
 class StackUnderflow(BaseException):
  pass
 
@@ -277,10 +192,32 @@ class StackRenderer():
   return str(self.styletree)
 
 
+
 class TestCompile(unittest.TestCase):
  def test_discriminate_nested(self):
   brains, members = cut_you_up('[(ffffff,310000) this will be [(200,30,30) ooh!] this will be [(200,30,30) aah!] this will be [(200,30,30) absolutely [(200,0,200) whee!]]]')
   self.assertEqual([is_styled_nest(i) for i in members], [False, True, False, True, False, True])
+
+ def test_ansi_compilation(self):
+  import renderers 
+  rendfunctions = [renderers.render_fore_color, renderers.render_back_color, renderers.render_bold, renderers.render_italic]
+  values = [
+        """[(ffffff,000000,1) this is no orthodox [(,555555) beheading] [(,,0) I'm cuting you [(ff0000) up] cutting you up] cutting you up will be so refreshing to me]""",
+        '[(ff0000,440000,1) bold red on dark red]',
+        '[(00ff00,440000,1) Im green over dark red and bold! [(0000ff) but I feel all blue]]',
+        '[(ff00ff,000000) [(,bbbb00) magenta over yellow] [(,00bbbb) magenta over cyan] [(,0000bb) magenta over blue] just magenta]',
+        '[(ffffff,007700,0,0) white over green darkish [(,,1) Im just bold [(,,,1) Im bold and italic!] just bold again] Ive been restored to my previous glory]'
+        ]
+
+  results = [
+        "\x1b[38;2;255;255;255m\x1b[48;2;0;0;0m\x1b[1mthis is no orthodox \x1b[48;2;85;85;85mbeheading\x1b[48;2;0;0;0m \x1b[22mI'm cuting you \x1b[38;2;255;0;0mup\x1b[38;2;255;255;255m cutting you up\x1b[1m cutting you up will be so refreshing to me",
+        '\x1b[38;2;255;0;0m\x1b[48;2;68;0;0m\x1b[1mbold red on dark red',
+        '\x1b[38;2;0;255;0m\x1b[48;2;68;0;0m\x1b[1mIm green over dark red and bold! \x1b[38;2;0;0;255mbut I feel all blue',
+        '\x1b[38;2;255;0;255m\x1b[48;2;187;187;0mmagenta over yellow\x1b[48;2;0;0;0m \x1b[48;2;0;187;187mmagenta over cyan\x1b[48;2;0;0;0m \x1b[48;2;0;0;187mmagenta over blue\x1b[48;2;0;0;0m just magenta',
+        '\x1b[38;2;255;255;255m\x1b[48;2;0;119;0m\x1b[22m\x1b[23mwhite over green darkish \x1b[1mIm just bold \x1b[3mIm bold and italic!\x1b[23m just bold again\x1b[22m Ive been restored to my previous glory'
+        ]
+  obj = StackRenderer('not important')
+  self.assertEqual([obj.compa(i, rendfunctions) for i in values], results)
 
 if __name__ == "__main__":
  unittest.main()
